16aff3f9c3215c163c86c31a00806f0196cbba22
Jonathan Lee <jonathancklee@gmail.com>
2026-07-10 12:26:18 +1000

    Add error handle for jikan api

--- src/utils/data-hooks/useJikan.ts
+++ src/utils/data-hooks/useJikan.ts
@@ -4,21 +4,41 @@ export function useJikan<TData = unknown>(
     endpoint: string,
     params: Record<string, string> = {},
     options?: Omit<
-        UseQueryOptions<unknown, unknown, TData>,
+        UseQueryOptions<unknown, Error & { status?: number }, TData>,
         "queryKey" | "queryFn"
     >,
 ) {
     const url = new URL(`https://api.jikan.moe/v4/${endpoint}`);
-
     Object.entries(params).forEach(([key, value]) => {
         url.searchParams.set(key, value);
     });
 
-    return useQuery<unknown, unknown, TData>({
+    return useQuery<unknown, Error & { status?: number }, TData>({
         queryKey: [endpoint, params],
-        queryFn: () => fetch(url).then((res) => res.json()),
+        queryFn: () => fetchJikan(url.toString()),
         staleTime: 5 * 60 * 1000,
         gcTime: 10 * 60 * 1000,
+        retry: (error) => {
+            if (error === 504) {
+                return true;
+            }
+            return false;
+        },
         ...options,
     });
 }
+
+const fetchJikan = async (url: string) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+        // Create an error and append the status code for React Query to read
+        const error = new Error(
+            `Something went wrong with the Jikan API: ${res.statusText}. Please try again later.`,
+        ) as Error & { status: number };
+        error.status = res.status;
+        throw error;
+    }
+
+    return res.json();
+};
